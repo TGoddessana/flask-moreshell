1f881aff1cb4609e5f217381adf8579c1b0b2b82
tgoddessana <twicegoddessana1229@gmail.com>
2023-11-18 15:37:44 +0900

    test: change test code run

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,7 +2,7 @@
 import pytest
 from click.testing import CliRunner
 
-from flask_moreshell import __main__
+from flask_moreshell import shell
 
 
 @pytest.fixture
@@ -13,5 +13,5 @@ def runner() -> CliRunner:
 
 def test_main_succeeds(runner: CliRunner) -> None:
     """It exits with a status code of zero."""
-    result = runner.invoke(__main__.main)
+    result = runner.invoke(shell)
     assert result.exit_code == 0
