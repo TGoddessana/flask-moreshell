5c3fa5ee63482535ad3601d692fbe6ed999c52d8
goddessana <twicegoddessana1229@gmail.com>
2024-02-11 17:33:36 +0900

    test: add type hints in tests

--- tests/shells/test_bpython_shell.py
+++ tests/shells/test_bpython_shell.py
@@ -6,13 +6,13 @@ from flask_moreshell.shells.bpython_shell import BPythonShell
 
 
 @pytest.fixture
-def bpython_shell():
+def bpython_shell() -> BPythonShell:
     return BPythonShell()
 
 
-def test_get_shell_name(bpython_shell):
+def test_get_shell_name(bpython_shell: BPythonShell) -> None:
     assert bpython_shell.get_shell_name() == "BPython"
 
 
-def test_get_shell_version(bpython_shell):
+def test_get_shell_version(bpython_shell: BPythonShell) -> None:
     assert bpython_shell.get_shell_version() == version("bpython")
--- tests/shells/test_ipython_shell.py
+++ tests/shells/test_ipython_shell.py
@@ -4,15 +4,15 @@ from flask_moreshell.shells.ipython_shell import IPythonShell
 
 
 @pytest.fixture
-def ipython_shell():
+def ipython_shell() -> IPythonShell:
     return IPythonShell()
 
 
-def test_get_shell_name(ipython_shell):
+def test_get_shell_name(ipython_shell: IPythonShell) -> None:
     assert ipython_shell.get_shell_name() == "IPython"
 
 
-def test_get_shell_version(ipython_shell):
+def test_get_shell_version(ipython_shell: IPythonShell) -> None:
     from IPython import __version__
 
     assert ipython_shell.get_shell_version() == __version__
