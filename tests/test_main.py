d1d994db5fc6a0ccd9db2ae0f835a59ca741fe91
goddessana <twicegoddessana1229@gmail.com>
2024-02-11 17:34:04 +0900

    refactor: remove unnecessary comments

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,40 +6,32 @@ from flask_moreshell import shell
 
 
 @pytest.fixture
-def app():
+def app() -> Flask:
     app = Flask(__name__)
     app.config["TESTING"] = True
     return app
 
 
 @pytest.fixture
-def runner(app):
+def runner(app: Flask) -> CliRunner:
     return CliRunner()
 
 
-def test_ipython_shell(runner, app):
+def test_ipython_shell(runner: CliRunner, app: Flask) -> None:
     with app.app_context():
         result = runner.invoke(shell, ["--shelltype", "ipython"])
     assert result.exit_code == 0
     assert "IPython" in result.output
 
 
-# def test_bpython_shell(runner, app):
-#     with app.app_context():
-#         result = runner.invoke(shell, ["--shelltype", "bpython"])
-#         assert result.output == ""
-#     assert result.exit_code == 0
-#     assert "BPython" in result.output
-
-
-def test_pypython_shell(runner, app):
+def test_ptpython_shell(runner: CliRunner, app: Flask) -> None:
     with app.app_context():
         result = runner.invoke(shell, ["--shelltype", "ptpython"])
     assert result.exit_code == 0
     assert "PTPython" in result.output
 
 
-def test_python_shell(runner, app):
+def test_python_shell(runner: CliRunner, app: Flask) -> None:
     with app.app_context():
         result = runner.invoke(shell, ["--shelltype", "python"])
     assert result.exit_code == 0
